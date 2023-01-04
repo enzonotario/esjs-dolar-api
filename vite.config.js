535025b808ed4243a75f1545b96777afc002ef8c
enzonotario <enzo.notario@gmail.com>
2023-01-04 02:32:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import EsJS from '@es-js/vite-plugin-esjs'
+import vavite from "vavite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // https://github.com/es-js/esjs
+    EsJS(),
+
+    // https://github.com/cyco130/vavite
+    vavite({
+      serverEntry: './src/app.esjs',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.esjs',
+    ],
+  },
+  build: {
+    outDir: './dist'
+  },
+})
