ab79443ad1ede70a78f357c3e53acf2834152ec0
Enzo Notario <ernotario@ucasal.edu.ar>
2023-10-28 21:47:37 -0300

    chore(esjs) prettier

--- tests/acciones/guardado/cerrarCotizaciones.test.js
+++ tests/acciones/guardado/cerrarCotizaciones.test.js
@@ -1,4 +1,4 @@
-import { it, expect } from 'vitest'
+import { expect, it } from 'vitest'
 import { cerrarCotizaciones } from '@/acciones/guardado/cerrarCotizaciones.esjs'
 import { consultarCotizacionPorMoneda } from '@/acciones/consulta/consultarCotizacionPorMoneda.esjs'
 import { monedasSoportadas } from '@/constantes.esjs'
