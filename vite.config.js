3a78e1c0d90cecf6f3ea40fa957318e799c2d186
Enzo Notario <ernotario@ucasal.edu.ar>
2024-01-27 11:40:45 -0300

    feat(api) cron

---
+++ vite.config.js
@@ -0,0 +1,39 @@
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import EsJS from '@es-js/vite-plugin-esjs'
+import devServer from '@hono/vite-dev-server'
+
+export default defineConfig({
+  plugins: [
+    // https://github.com/es-js/esjs
+    EsJS(),
+
+    devServer({
+      entry: './api/api.esjs',
+    }),
+  ],
+
+  test: {},
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./api', import.meta.url)),
+    },
+    extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.esjs'],
+  },
+
+  build: {
+    outDir: './dist',
+    emptyOutDir: false,
+    rollupOptions: {
+      input: './api/api.esjs',
+      output: {
+        format: 'esm',
+        entryFileNames: 'servidor/[name].js',
+        chunkFileNames: 'servidor/[name].js',
+        assetFileNames: 'servidor/[name].[ext]',
+        exports: 'auto',
+      },
+    },
+  },
+})
