f9a6886150421f67e3ff43ecf15e495a460e3451
enzonotario <enzo.notario@gmail.com>
2024-05-23 23:48:42 -0300

    chore(docs) regions

--- docs/.vitepress/theme/components/SelectRegion.vue
+++ docs/.vitepress/theme/components/SelectRegion.vue
@@ -14,13 +14,13 @@ const options = region.regions
     <label for="region" class="region">Región</label>
     <select
       id="region"
-      :value="region.currentRegion.value.code"
       class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
       @change="region.onRegionChange($event, router)"
     >
       <option
         v-for="option in options" :key="option.code"
         :value="option.code"
+        :selected="option.code === region.currentRegion.value.code"
       >
         {{ option.name }}
       </option>
--- docs/.vitepress/theme/components/SelectRegionSidebarItem.vue
+++ docs/.vitepress/theme/components/SelectRegionSidebarItem.vue
@@ -14,7 +14,6 @@ const options = region.regions
     <label for="region-sidebar" class="block my-1 text-sm font-medium text-gray-900 dark:text-white">Región</label>
     <select
       id="region"
-      :value="region.currentRegion.value.code"
       class="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
       @change="region.onRegionChange($event, router)"
     >
@@ -22,6 +21,7 @@ const options = region.regions
         v-for="option in options"
         :key="option.code"
         :value="option.code"
+        :selected="option.code === region.currentRegion.value.code"
       >
         {{ option.name }}
       </option>
