79fc845751314164aaabf51c4cf6c065e32bdc10
enzonotario <enzo.notario@gmail.com>
2024-06-04 22:45:05 -0300

    feat(cron) mx

---
+++ tests/mx/mx.test.js
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest'
+import extraer from '@/mx/banxico.extractor.esjs'
+
+describe('mx.dolarapi.com', () => {
+  it('extraer dolares', async () => {
+    const dolar = await extraer()
+
+    expect(dolar).toBeTypeOf('object')
+    expect(dolar.moneda).toBeTypeOf('string')
+    expect(dolar.fix).toBeTypeOf('number')
+    expect(dolar.compra).toBeTypeOf('number')
+    expect(dolar.venta).toBeTypeOf('number')
+    expect(dolar.fechaActualizacion).toBeTypeOf('string')
+  }, {
+    timeout: 10000,
+  })
+})
