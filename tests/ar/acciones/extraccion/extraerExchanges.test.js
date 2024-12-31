a218088212b794ebe79531adb96bbeacdb3301e8
enzonotario <enzo.notario@gmail.com>
2024-12-31 15:25:41 -0300

    feat(exchanges): satoshitango

--- tests/ar/acciones/extraccion/extraerExchanges.test.js
+++ tests/ar/acciones/extraccion/extraerExchanges.test.js
@@ -5,15 +5,25 @@ describe('extraerExchanges', () => {
   it('extrae cotizaciones de exchanges', async () => {
     const cotizaciones = await extraerExchanges()
 
+    const cotizacionesAtributos = cotizaciones.map(cotizacion => ({
+      exchange: cotizacion.exchange,
+      par: cotizacion.par,
+      parCriptomoneda: cotizacion.parCriptomoneda,
+    }))
+
+    console.table(cotizacionesAtributos)
+
+    expect(cotizacionesAtributos).toMatchSnapshot()
 
     expect(cotizaciones.length).toBeGreaterThan(0)
     for (const cotizacion of cotizaciones) {
       expect(cotizacion).toBeTypeOf('object')
 
-      if (cotizacion.compra)
-        expect(cotizacion.compra).toBeTypeOf('number')
+      expect(cotizacion.compra).toBeTypeOf('number')
 
-      expect(cotizacion.venta).toBeTypeOf('number')
+      if (cotizacion.venta)
+        expect(cotizacion.venta).toBeTypeOf('number')
+
       expect(cotizacion.par).toBeTypeOf('object')
 
       if (cotizacion.parCriptomoneda)
