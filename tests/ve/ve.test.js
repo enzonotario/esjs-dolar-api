58486b7327d1f13e1c9720d022b81dcc4a7ecae0
enzonotario <enzo.notario@gmail.com>
2025-05-13 23:11:14 -0300

    change(ve): extraer Paralelo desde Las Lucas
    Closes #46

--- tests/ve/ve.test.js
+++ tests/ve/ve.test.js
@@ -1,9 +1,10 @@
 import { describe, expect, it } from 'vitest'
-import extraer from '@/ve/monitordolarvenezuela.extractor.esjs'
+import extraerMonitor from '@/ve/monitordolarvenezuela.extractor.esjs'
+import extraerLasLucas from '@/ve/laslucas.extractor.esjs'
 
 describe('ve.dolarapi.com', () => {
-  it('extraer dolares', async () => {
-    const cotizaciones = await extraer()
+  it('extraer dolares de Monitor Dolar Venezuela', async () => {
+    const cotizaciones = await extraerMonitor()
 
     expect(cotizaciones.length).toBeGreaterThan(0)
 
@@ -27,6 +28,25 @@ describe('ve.dolarapi.com', () => {
       {
         fuente: 'bitcoin',
         nombre: 'Bitcoin',
+        compra: null,
+        venta: null,
+        promedio: expect.any(Number),
+        fechaActualizacion: expect.any(String),
+      },
+    ])
+  }, {
+    timeout: 10000,
+  })
+
+  it('extraer dolares de Las Lucas', async () => {
+    const cotizaciones = await extraerLasLucas()
+
+    expect(cotizaciones.length).toBeGreaterThan(0)
+
+    expect(cotizaciones).toMatchObject([
+      {
+        fuente: 'paralelo',
+        nombre: 'Paralelo',
         compra: null,
         venta: null,
         promedio: expect.any(Number),
