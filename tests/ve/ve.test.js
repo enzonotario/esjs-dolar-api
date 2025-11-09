dd3b3132cc5c8320388afe145b771bbd8408c284
enzonotario <enzo.notario@gmail.com>
2025-11-09 19:42:29 -0300

    feat(extractors): refactor Las Lucas extractor for improved data retrieval and structure

--- tests/ve/ve.test.js
+++ tests/ve/ve.test.js
@@ -1,43 +1,7 @@
 import { describe, expect, it } from 'vitest'
-import extraerMonitor from '@/ve/monitordolarvenezuela.extractor.esjs'
 import extraerLasLucas from '@/ve/laslucas.extractor.esjs'
 
 describe('ve.dolarapi.com', () => {
-  it('extraer dolares de Monitor Dolar Venezuela', async () => {
-    const cotizaciones = await extraerMonitor()
-
-    expect(cotizaciones.length).toBeGreaterThan(0)
-
-    expect(cotizaciones).toMatchObject([
-      {
-        fuente: 'oficial',
-        nombre: 'Oficial',
-        compra: null,
-        venta: null,
-        promedio: expect.any(Number),
-        fechaActualizacion: expect.any(String),
-      },
-      {
-        fuente: 'paralelo',
-        nombre: 'Paralelo',
-        compra: null,
-        venta: null,
-        promedio: expect.any(Number),
-        fechaActualizacion: expect.any(String),
-      },
-      {
-        fuente: 'bitcoin',
-        nombre: 'Bitcoin',
-        compra: null,
-        venta: null,
-        promedio: expect.any(Number),
-        fechaActualizacion: expect.any(String),
-      },
-    ])
-  }, {
-    timeout: 10000,
-  })
-
   it('extraer dolares de Las Lucas', async () => {
     const cotizaciones = await extraerLasLucas()
 
@@ -52,8 +16,16 @@ describe('ve.dolarapi.com', () => {
         promedio: expect.any(Number),
         fechaActualizacion: expect.any(String),
       },
+      {
+        fuente: 'oficial',
+        nombre: 'Oficial',
+        compra: null,
+        venta: null,
+        promedio: expect.any(Number),
+        fechaActualizacion: expect.any(String),
+      },
     ])
   }, {
-    timeout: 10000,
+    timeout: 20000,
   })
 })
